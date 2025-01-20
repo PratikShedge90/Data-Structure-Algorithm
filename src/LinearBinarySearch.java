e9b31e6d43aa8c9c6d9349bc5a54313ee61c4202
Pratik <pratikshedge90@gmail.com>
2025-01-20 11:01:21 +0530

    Linear Search Algorithm

--- src/LinearBinarySearch.java
+++ src/LinearBinarySearch.java
@@ -14,7 +14,6 @@ public class LinearBinarySearch {
 
 
     }
-
     private static int linearSearch(int[] nums, int target) {
         for (int i=0; i< nums.length; i++ ) {
             if(nums[i] == target) {
--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class LinearSearch {
-}
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {5,11,13,64,99,88};
+
+        int target = 99;
+
+        int result = linearSearch(arr, target);
+        if(result != -1){
+            System.out.println("Element Found at Index : " + result);
+        }else{
+            System.out.println("Element Not Found ");
+        }
+
+        }
+        public static int linearSearch(int[] arr, int target)
+        {
+
+            for (int i=0; i< arr.length; i++)
+            {
+                if (arr[i] == target) {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+
+    }
+
+
+
